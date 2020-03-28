0770266ed2a3a69ed11e6916acc3598ee255ce6e
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-03-28 17:17:48 +0530

    added find_cube function

--- basic_functions.c
+++ basic_functions.c
@@ -3,6 +3,7 @@
 unsigned char is_even(int);
 unsigned char is_odd(int);
 long int find_square(int);
+long int find_cube(int);
 
 unsigned char is_even(int number) {
   return number % 2 == 0;
@@ -16,6 +17,10 @@ long int find_square(int number) {
   return (number * number);
 };
 
+long int find_cube(int number) {
+  return (number * find_square(number));
+};
+
 int main(void)
 {
   printf("Enter a number: ");
@@ -23,9 +28,10 @@ int main(void)
   int number;
   scanf("%d",&number);
 
-  printf("%d is %s number\n", number, is_even(number) ? "even" : "not even");
-  printf("%d is %s number\n", number, is_odd(number) ? "odd" : "not odd");
+  printf("%d is a %s number\n", number, is_even(number) ? "even" : "not even");
+  printf("%d is a %s number\n", number, is_odd(number) ? "odd" : "not odd");
   printf("square of given number '%d' is %ld\n", number, find_square(number));
+  printf("cube of given number '%d' is %ld\n", number, find_cube(number));
 
   return 0;
 };
