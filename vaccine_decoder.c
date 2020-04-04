3e95756b716ccc09eb457bafa2eb4c48dc8650c8
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-04-04 15:42:36 +0530

    modified vaccine_decoder

--- vaccine_decoder.c
+++ vaccine_decoder.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
-void decrypt(int *encrypted_code, int length)
+int decrypt_char(int char_ascii_code, int min, int max)
 {
-  int decrypted_code[length];
+  char_ascii_code = char_ascii_code + 13;
+  if(char_ascii_code > max){
+     char_ascii_code = min + (char_ascii_code - max);
+  }
+  return char_ascii_code;
+}
+
+void decrypt(char *encrypted_code, int length)
+{
+  char decrypted_code[length];
   for(int count = 0; count < length; count++){
    decrypted_code[count] = encrypted_code[count];
-      if(decrypted_code[count] != 32){
-        decrypted_code[count] = decrypted_code[count] + 13;
-      }
-      if(decrypted_code[count]>122){
-        decrypted_code[count] = 96 + (decrypted_code[count]-122);
-      }
+   if(encrypted_code[count] > 64 && encrypted_code[count] <= 90){
+     decrypted_code[count] = decrypt_char(encrypted_code[count], 64, 90);
+   }
+   if(encrypted_code[count] > 96 && encrypted_code[count] <= 122){
+     decrypted_code[count] = decrypt_char(encrypted_code[count],96,122);
+   }
     printf("%c",decrypted_code[count]);
   }
   printf("\n");
-};
+}
 
-int main(void){
-  int encrypted_code1[9] = {116, 98, 32, 112, 98, 101, 98, 97, 110};
-  printf("\nDecrypted Code-->\n");
+
+int main(void)
+{
+  char encrypted_code1[9] = {116,98,32,112,98,101,98,97,110};
+  char encrypted_code2[20] = {'C','e','v','l','n','a','f','u','h',' ','L','b','h',' ','Q','v','q',' ','V','g'};
   decrypt(encrypted_code1, 9);
+  decrypt(encrypted_code2, 20);
   return 0;
-};
+}
